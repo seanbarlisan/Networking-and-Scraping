7bf4ba75bb8032b363cb0a8676534a401f7eb53b
seanbarlisan <smbarlisan@gmail.com>
2024-12-09 15:20:51 -0500

    modifed to see GET requests and results from running python code and write into CSV

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-import requests 
+import requests
 from requests.exceptions import HTTPError
+import csv
 
-URLs = ['https://www.google.com/', 'https://www.amazon.com/invalid']
+# List of URLs to check
+URLs = [
+    'https://www.google.com/',
+    'https://www.amazon.com/invalid',
+    'https://www.mscdirect.com/',
+    'https://www.amazon.com/',
+    'https://www.mscdirect.com/browse/Holemaking?navid=2106067'
+]
 
-for url in URLs:
-    try:
-        response = requests.get(url) # obtain the response from the GET request for URL
-        response.raise_for_status() # raise the response status, can give errors if found
-    except HTTPError as http_err: # if there is an error for the response
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err: # if there is an exception for the response
-        print(f"Other error occurred: {err}")
-    else:
-        print("Success!")
-        print(response.json)
+# CSV file name
+output_file = "url_results.csv"
+
+# Open the CSV file for writing
+with open(output_file, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    # Write the header row
+    writer.writerow(['URL', 'Status Code', 'Result'])
+
+    # Process each URL
+    for url in URLs:
+        try:
+            # Send GET request
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        except HTTPError as http_err:
+            print(f"HTTP error occurred for {url}: {http_err}")
+            writer.writerow([url, response.status_code if 'response' in locals() else 'N/A', f"HTTP Error: {http_err}"])
+        except Exception as err:
+            print(f"Other error occurred for {url}: {err}")
+            writer.writerow([url, 'N/A', f"Other Error: {err}"])
+        else:
+            print(f"Success for {url}")
+            writer.writerow([url, response.status_code, "Success"])
